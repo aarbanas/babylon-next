e2dd321ca9523323a6b1248fa7d0b4fd0b1738d2
Andrej <andrej.arbanas@q.agency>
2023-08-23 12:43:49 +0200

    style: redesign register page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,6 +6,7 @@ import login from '@/services/auth/login';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Button from '@/shared/button/button';
+import Back from '@/shared/icons/Back';
 
 type FormData = {
   username: string;
@@ -40,7 +41,7 @@ const Login: NextPage = () => {
   return (
     <>
       <div className="flex flex-col md:flex-row justify-center items-center h-full sm:p-0 md:p-6 lg:p-8">
-        <div className="flex w-2/5">
+        <div className="flex flex-col items-center w-2/5">
           <Image
             src="/login_page.png"
             alt="Login image"
@@ -49,7 +50,16 @@ const Login: NextPage = () => {
             width={100}
             height={100}
           />
+          <div
+            className="flex justify-start cursor-pointer"
+            onClick={() => router.push('/')}>
+            <div className="w-8 h-8">
+              <Back />
+            </div>
+            <span className="ml-2 flex items-center">Povratak</span>
+          </div>
         </div>
+
         <div className="flex flex-col w-3/5 items-center">
           <form
             className="mt-6 w-full md:w-3/5"
@@ -63,29 +73,31 @@ const Login: NextPage = () => {
               <input
                 {...register('username', { required: true })}
                 type="email"
-                className="block w-full px-4 py-2 mt-2 bg-white border-gray-300 rounded-xl focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-4 py-2 mt-3 bg-white border-gray-300 rounded-xl focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
             <div className="mb-2">
-              <label className="block text-sm font-light text-gray-500">
+              <label className="block text-sm font-light text-gray-500 mt-1">
                 Lozinka
               </label>
               <input
                 {...register('password', { required: true })}
                 type="password"
-                className="block w-full px-4 py-2 mt-2 bg-white border-gray-300 rounded-xl focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
+                className="block w-full px-4 py-2 mt-3 bg-white border-gray-300 rounded-xl focus:border-red-400 focus:ring-red-300 focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
-            <a href="#" className="text-xs underline hover:text-red-700">
+
+            <a href="#" className="text-xs underline hover:text-red-700 mt-3">
               Zaboravljena Lozinka?
             </a>
-            <div className="mt-6">
+
+            <div className="mt-4 md:mt-8">
               <Button color="primary" disabled={submitting} type="submit" full>
                 Prijava
               </Button>
             </div>
 
-            <p className="mt-6 mb-6 text-xs font-light text-center text-gray-700">
+            <p className="mt-4 md:mt-8 mb-6 md:mb-0 text-xs font-light text-center text-gray-700">
               {' '}
               Nemaš račun?{' '}
               <a
