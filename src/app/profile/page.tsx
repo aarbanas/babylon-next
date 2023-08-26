f5b8d7be66ab2a7d41b8f367f0a492ac1aa5334b
Ivan Miljancic <ivan.miljancic@gmail.com>
2023-08-26 09:46:18 +0200

    feat: create NavigationTabs component

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,26 +1,47 @@
 'use client';
+
+import Link from 'next/link';
 import { NextPage } from 'next';
+import { HiPencil, HiDocumentAdd } from 'react-icons/hi';
+
+import { Role } from '../register/types';
+import { UserDto } from '@/services/user/dto/user.dto';
+import { LoadingSpinner } from '@/shared/loadingSpinner';
+import { NavigationTabs } from './components/navigationTabs';
 import DashboardLayout from '@/shared/layouts/dashboardLayout';
 import { useUserSession } from '@/services/auth/useUserSession';
-import { Role } from '../register/types';
 import { UserProfileForm } from './components/userProfileForm';
 import { OrganisationProfileForm } from './components/organisationProfileForm';
-import { LoadingSpinner } from '@/shared/loadingSpinner';
+
+const profileUpdateFormFactory = (role: Role, userSession: UserDto) => {
+  if (role === Role.USER) {
+    return <UserProfileForm userData={userSession} />;
+  }
+
+  return <OrganisationProfileForm userData={userSession} />;
+};
 
 const Profile: NextPage = () => {
   const userSession = useUserSession();
 
-  if (!userSession) {
-    return <LoadingSpinner />;
-  }
-
   return (
     <>
       <DashboardLayout>
-        {userSession.role === Role.USER ? (
-          <UserProfileForm userData={userSession} />
+        <NavigationTabs>
+          <Link href="/profile" className="flex items-center">
+            <HiPencil className="mr-2" size={24} />
+            <span>Osnovni podatci</span>
+          </Link>
+          <Link href="/profile/certificates" className="flex items-center">
+            <HiDocumentAdd className="mr-2" size={24} />
+            <span>Certifikati</span>
+          </Link>
+        </NavigationTabs>
+
+        {!userSession ? (
+          <LoadingSpinner width={64} height={64} />
         ) : (
-          <OrganisationProfileForm userData={userSession} />
+          profileUpdateFormFactory(userSession.role, userSession)
         )}
       </DashboardLayout>
     </>
