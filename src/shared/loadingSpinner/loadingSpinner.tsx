6b7e6ec902ef655f6ea84f7da769b7b9df2b0077
Ivan Miljancic <ivan.miljancic@gmail.com>
2023-08-26 13:21:50 +0200

    feat: return react icons package

--- src/shared/loadingSpinner/loadingSpinner.tsx
+++ src/shared/loadingSpinner/loadingSpinner.tsx
@@ -34,7 +34,7 @@ const LoadingSpinner: FC<LoadingSpinnerProps> = ({
               d="M50 15A35 35 0 1 0 74.74873734152916 25.251262658470843"
               fill="none"
               stroke={color}
-              stroke-width="12"
+              strokeWidth="12"
             />
             <path d="M49 3L49 27L61 15L49 3" fill={color} />
             <animateTransform
