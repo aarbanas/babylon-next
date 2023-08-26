9d094c70e324658a4775543697ca33053149b3a1
Andrej <andrej.arbanas@q.agency>
2023-08-26 12:43:59 +0200

    feat: add header components

---
+++ src/shared/header/Header.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+const Header: NextPage = () => {
+  return (
+    <>
+      <div className="flex ml-auto p-12">
+        <span>Pozdrav, </span>
+      </div>
+    </>
+  );
+};
+
+export default Header;
