cff354a3ba5dd3f802ab67edc75c3d05fc2f2927
Andrej <andrej.arbanas@q.agency>
2023-08-26 19:55:30 +0200

    feat: add header; allow images to be displayed from https

--- src/shared/header/Header.tsx
+++ src/shared/header/Header.tsx
@@ -1,10 +1,26 @@
 import { NextPage } from 'next';
+import styles from './Header.module.scss';
+import Image from 'next/image';
 
-const Header: NextPage = () => {
+type Props = {
+  username: string;
+  photo: string;
+};
+
+const Header: NextPage<Props> = ({ username, photo }) => {
   return (
     <>
-      <div className="flex ml-auto p-12">
-        <span>Pozdrav, </span>
+      <div className={styles.container}>
+        <div className={styles.avatarContainer}>
+          <Image
+            src={photo}
+            style={{ borderRadius: '100000px' }}
+            alt="User profile picture"
+            width={40}
+            height={40}
+          />
+          <span className={styles.username}>Pozdrav, {username}</span>
+        </div>
       </div>
     </>
   );
