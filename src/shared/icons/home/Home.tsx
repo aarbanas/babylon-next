1ca73e6ba41b061f4abbb57386afef453e1039b6
Andrej <andrej.arbanas@q.agency>
2023-08-26 12:49:32 +0200

    feat: add dynamic size for icons

--- src/shared/icons/home/Home.tsx
+++ src/shared/icons/home/Home.tsx
@@ -1,9 +1,11 @@
-const HomeIcon = () => {
+import { FC } from 'react';
+
+const HomeIcon: FC<{ size?: number }> = ({ size = 24 }) => {
   return (
     <svg
       fill="rgb(255, 255, 255)"
-      width="24px"
-      height="24px"
+      width={size}
+      height={size}
       viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="none" />
       <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
