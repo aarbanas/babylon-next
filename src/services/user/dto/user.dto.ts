0f35a5dc153e2ee134bc7b26f4a586b0e9f0382f
Andrej <andrej.arbanas@q.agency>
2023-08-30 15:24:57 +0200

    fix: change Layout to DashboardLayout; remove duplicate type

--- src/services/user/dto/user.dto.ts
+++ src/services/user/dto/user.dto.ts
@@ -1,4 +1,4 @@
-import { Role } from '@/app/register/types';
+import { Role, Type } from '@/app/register/types';
 
 export type UserDto = {
   userAttributes: UserAttributesDto;
@@ -16,7 +16,7 @@ export type UserAttributesDto = {
   lastname: string;
   city: string;
   phone: string;
-  type: UserType;
+  type: Type;
 };
 
 type OrganisationAttributesDto = {
@@ -26,5 +26,3 @@ type OrganisationAttributesDto = {
   city: string;
   oib: string;
 };
-
-export type UserType = 'DOCTOR' | 'NURSE' | 'LIFEGUARD';
