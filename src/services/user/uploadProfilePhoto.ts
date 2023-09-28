c33e6f68450398b69ad98be036804e259af48e63
Ivan Miljančić <ivan.miljancic@gmail.com>
2023-09-28 20:57:26 +0200

    [Feature] Upload Profile Photo Form (#10)
    * feat: create image preview component on profile screen
    
    * feat: create upload methods
    
    * feat: basic modal component created
    
    * refactor: rename component for showing profile picture
    
    * feat: initial profile picture update form
    
    * refactor: cleanup ProfilePhoto update components
    
    * refactor: use create object URL to replace current picture
    
    * fix: make the image editor form responsive
    
    * feat: add a persistent flag to the modal

---
+++ src/services/user/uploadProfilePhoto.ts
@@ -0,0 +1,7 @@
+import { APIs, post } from "@/shared/http-service/httpService"
+
+const uploadProfilePhoto = async (formData: FormData) => {
+  return post<{profilePhoto: string}>(`${APIs.USER}/upload-avatar`, formData);
+}
+
+export default uploadProfilePhoto;
