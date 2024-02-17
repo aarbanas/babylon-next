d685f79f5495f297ed377607cd4fbad0fe0bd390
Andrej <andrej.arbanas@q.agency>
2024-02-17 22:38:11 +0100

    feat: add middleware protection for admin routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { authRoutes, protectedRoutes } from '@/routes/routes';
+import jwt_decode from 'jwt-decode';
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('currentUser')?.value;
-
+  console.log(request.nextUrl.pathname);
   if (
     protectedRoutes.includes(request.nextUrl.pathname) &&
     (!currentUser || Date.now() > JSON.parse(currentUser).expiredAt)
@@ -19,4 +20,24 @@ export function middleware(request: NextRequest) {
   if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
+
+  if (
+    request.nextUrl.pathname.startsWith('/admin') &&
+    (!currentUser ||
+      Date.now() > JSON.parse(currentUser).expiredAt ||
+      getRoleFromJWT(JSON.parse(currentUser).accessToken) !== 'ADMIN')
+  ) {
+    request.cookies.delete('currentUser');
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.delete('currentUser');
+
+    return response;
+  }
 }
+
+const getRoleFromJWT = (token: string) => {
+  const decodedToken = jwt_decode<{ role: string }>(token);
+  if (!decodedToken) return null;
+
+  return decodedToken.role;
+};
