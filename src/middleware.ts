286c6e58682c3385c9a7e49075d3a423293ab828
Andrej Arbanas <andrej.arbanas@q.agency>
2024-02-29 09:30:38 +0100

    Feat/admin users (#23)
    * feat: add admin users with components from v0
    ---------
    Co-authored-by: Ivan Miljančić <ivan.miljancic@gmail.com>

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
-import { authRoutes, protectedRoutes } from '@/routes/routes';
+import { authAdminRoutes, authRoutes, protectedRoutes } from '@/routes/routes';
+import jwt_decode from 'jwt-decode';
 
 export function middleware(request: NextRequest) {
   const currentUser = request.cookies.get('currentUser')?.value;
@@ -19,4 +20,28 @@ export function middleware(request: NextRequest) {
   if (authRoutes.includes(request.nextUrl.pathname) && currentUser) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
+
+  if (
+    request.nextUrl.pathname.startsWith('/admin') &&
+    (!currentUser ||
+      Date.now() > JSON.parse(currentUser).expiredAt ||
+      getRoleFromJWT(JSON.parse(currentUser).accessToken) !== 'ADMIN')
+  ) {
+    request.cookies.delete('currentUser');
+    const response = NextResponse.redirect(new URL('/login', request.url));
+    response.cookies.delete('currentUser');
+
+    return response;
+  }
+
+  if (authAdminRoutes.includes(request.nextUrl.pathname) && currentUser) {
+    return NextResponse.redirect(new URL('/admin/users', request.url));
+  }
 }
+
+const getRoleFromJWT = (token: string) => {
+  const decodedToken = jwt_decode<{ role: string }>(token);
+  if (!decodedToken) return null;
+
+  return decodedToken.role;
+};
