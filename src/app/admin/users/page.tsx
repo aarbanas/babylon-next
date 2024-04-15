b3b489d7ccbbec20d617eda5105d49dcd0613f82
Andrej <andrej.arbanas@q.agency>
2024-04-15 10:19:42 +0200

    refactor: delete alertDialog and use Modal

--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -25,9 +25,9 @@ import { UserDto } from '@/services/user/dto/user.dto';
 import { ArrowUpDown, CheckCircle2, SearchIcon, XCircle } from 'lucide-react';
 import { debounce } from 'lodash';
 import AdminLayout from '@/shared/layouts/adminLayout';
-import AlertDialog from '@/components/ui/alertDialog';
 import { toast } from 'react-toastify';
 import deleteUser from '@/services/user/delete';
+import Modal from '@/shared/modal/Modal';
 
 const UserList = () => {
   const [users, setUsers] = useState<UserDto[]>([]);
@@ -184,12 +184,35 @@ const UserList = () => {
                       Delete
                     </Button>
                     {isDialogOpen && (
-                      <AlertDialog
-                        title="Delete User"
-                        message={`Are you sure you want to delete user ID: ${userIdToDelete}.`}
-                        onClose={() => setDialogOpen(false)}
-                        onConfirm={() => _deleteUser(userIdToDelete!)}
-                      />
+                      <Modal
+                        isOpen={isDialogOpen}
+                        onClose={() => setDialogOpen(false)}>
+                        <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                          <h3 className="text-lg font-medium leading-6 text-gray-900">
+                            Delete User
+                          </h3>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                              Are you sure you want to delete user ID:{' '}
+                              {userIdToDelete}.
+                            </p>
+                          </div>
+                        </div>
+                        <div className="justify-center px-4 py-3 sm:flex sm:px-6">
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-slate-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:mt-0 sm:w-auto sm:text-sm"
+                            onClick={() => _deleteUser(userIdToDelete!)}>
+                            Yes
+                          </button>
+                          <button
+                            type="button"
+                            className="mt-3 inline-flex w-full justify-center rounded-md border border-transparent bg-red-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-red-500 focus:ring-offset-2 sm:ml-3 sm:mt-0 sm:w-auto sm:text-sm"
+                            onClick={() => setDialogOpen(false)}>
+                            Close
+                          </button>
+                        </div>
+                      </Modal>
                     )}
                   </TableCell>
                 </TableRow>
