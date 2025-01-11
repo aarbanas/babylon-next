d4dfbd19d558c760e25f7d803a4e23072f737af1
Andrej Arbanas <andrej.arbanas@porsche.digital>
2025-01-11 23:32:56 +0100

    fix: build

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -2,11 +2,12 @@
 import PasswordReset from '@/components/reset-password';
 import { useUserSession } from '@/services/auth/useUserSession';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-const ResetPassword = () => {
+const ResetPasswordPage = () => {
   const userSession = useUserSession();
   const searchParams = useSearchParams();
-  const token = searchParams.get('token');
+  const token = searchParams.get('token') || '';
 
   // If the user is not logged in, we assume they're here with a valid token
   if (!userSession && token) {
@@ -16,6 +17,14 @@ const ResetPassword = () => {
   // If the user is logged in, we don't allow password reset
   return (
     <div>You are already logged in. Please log out to reset your password.</div>
+  );
+};
+
+const ResetPassword = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordPage />
+    </Suspense>
   );
 };
 
